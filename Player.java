97ded81b0497865598120c3fcd8896faaee1b1a7
ASR <asrtheasr007@gmail.com>
2026-04-10 05:36:32 +0530

    Player class updated with its fields - player_name and score

---
+++ Player.java
@@ -0,0 +1,13 @@
+// import java.lang.foreign.SymbolLookup;
+import java.util.Scanner;
+public class Player {
+    String player_name;
+    int Score;
+
+
+
+    public static void main(String[] args) {
+        
+    }
+
+}
